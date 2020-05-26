745caf3e181c046e888bf57705bb216516789275
xzheng <371635013@qq.com>
2020-05-26 22:08:11 +0800

    add biweekly 17

---
+++ leetcode/src/com/xinzhe/contest/biweeekly/biweekly17/Leetcode_biweekly_1704.java
@@ -0,0 +1,16 @@
+package com.xinzhe.contest.biweeekly.biweekly17;
+
+/**
+ * @Author Xin
+ * @create 2020/5/26
+ * Title :
+ * Description :
+ * link :
+ * Level : Hard
+ * Comment 17双周赛04
+ */
+
+//todo
+public class Leetcode_biweekly_1704 {
+
+}
