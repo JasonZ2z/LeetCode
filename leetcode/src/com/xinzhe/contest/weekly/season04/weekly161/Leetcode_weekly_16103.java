14437f3adf54fbb61428c367dfec7b161954eb59
xzheng <371635013@qq.com>
2020-06-20 21:44:47 +0800

    daily checkin

---
+++ leetcode/src/com/xinzhe/contest/weekly/season04/weekly161/Leetcode_weekly_16103.java
@@ -0,0 +1,56 @@
+package com.xinzhe.contest.weekly.season04.weekly161;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author Xin
+ * @create 2020/6/20
+ * Title : 1249. 移除无效的括号
+ * Description : 给你一个由 '('、')' 和小写字母组成的字符串 s。
+ *              你需要从字符串中删除最少数目的 '(' 或者 ')' （可以删除任意位置的括号)，使得剩下的「括号字符串」有效。请返回任意一个合法字符串。
+ *      有效「括号字符串」应当符合以下 任意一条 要求：
+ *          空字符串或只包含小写字母的字符串
+ *          可以被写作 AB（A 连接 B）的字符串，其中 A 和 B 都是有效「括号字符串」
+ *          可以被写作 (A) 的字符串，其中 A 是一个有效的「括号字符串」
+ * link : https://leetcode-cn.com/problems/minimum-remove-to-make-valid-parentheses
+ * Level : Medium
+ * Comment 161周赛03
+ */
+
+public class Leetcode_weekly_16103 {
+    public static void main(String[] args) {
+        System.out.println(minRemoveToMakeValid("lee(t(c)o)de)"));
+        System.out.println(minRemoveToMakeValid("a)b(c)d"));
+        System.out.println(minRemoveToMakeValid("))(("));
+        System.out.println(minRemoveToMakeValid("(a(b(c)d)"));
+    }
+    public static String minRemoveToMakeValid(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if(!Character.isLetter(arr[i])) {
+                if(arr[i] == '(') {
+                    stack.push(i);
+                }else {
+                    if(stack.isEmpty()) {
+                        arr[i] = ' ';
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = ' ';
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr) {
+            if(c != ' ') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ leetcode/src/com/xinzhe/contest/weekly/season04/weekly161/Leetcode_weekly_16104.java
@@ -0,0 +1,27 @@
+package com.xinzhe.contest.weekly.season04.weekly161;
+
+/**
+ * @Author Xin
+ * @create 2020/6/20
+ * Title : 1250. 检查「好数组」
+ * Description : 给你一个正整数数组 nums，你需要从中任选一些子集，然后将子集中每一个数乘以一个 任意整数，并求出他们的和。
+ *              假如该和结果为 1，那么原数组就是一个「好数组」，则返回 True；否则请返回 False。
+ * link : https://leetcode-cn.com/problems/check-if-it-is-a-good-array
+ * Level : Hard
+ * Comment 161周赛04
+ */
+
+public class Leetcode_weekly_16104 {
+    public static void main(String[] args) {
+    }
+    private int gcd(int a, int b){
+        return a==0? b : gcd(b%a , a);
+    }
+    public boolean isGoodArray(int[] nums) {
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i ++){
+            ans = gcd(ans , nums[i]);
+        }
+        return ans == 1;
+    }
+}
