9c26208fbbcbca4f48cf31838f835cfd8932929a
xzheng <371635013@qq.com>
2020-06-27 22:03:04 +0800

    bfs

---
+++ leetcode/src/com/xinzhe/categories/solutions/bfs/medium/Leetcode207.java
@@ -0,0 +1,46 @@
+package com.xinzhe.categories.solutions.bfs.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author Xin
+ * @create 2020/6/27
+ * Title : 207. 课程表
+ * Description : 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+ *          在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+ *          给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+ * Level : Medium
+ */
+public class Leetcode207 {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        int[] indegress = new int[n];
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] p : prerequisites) {
+            indegress[p[0]]++;
+            list.get(p[1]).add(p[0]);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if(indegress[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            Integer pre = queue.poll();
+            n--;
+            for (Integer cur : list.get(pre)) {
+                if(--indegress[cur] == 0) {
+                    queue.add(cur);
+                }
+            }
+        }
+        return n == 0;
+    }
+}
---
+++ leetcode/src/com/xinzhe/categories/solutions/bfs/medium/Leetcode310.java
@@ -0,0 +1,52 @@
+package com.xinzhe.categories.solutions.bfs.medium;
+
+import java.util.*;
+
+/**
+ * @Author Xin
+ * @create 2020/6/27
+ * Title : 310. 最小高度树
+ * Description : 对于一个具有树特征的无向图，我们可选择任何一个节点作为根。图因此可以成为树，在所有可能的树中，具有最小高度的树被称为最小高度树。
+ *              给出这样的一个图，写出一个函数找到所有的最小高度树并返回他们的根节点。
+ * link : https://leetcode-cn.com/problems/minimum-height-trees
+ * Level : Medium
+ */
+public class Leetcode310 {
+    //todo need to review
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> res = new ArrayList<>();
+        Set<Integer>[] graph = new HashSet[n];
+        int[] degree = new int[n];
+        for (int i = 0; i < n; ++i) {
+            graph[i] = new HashSet<>();
+        }
+        for (int[] edge : edges) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            if(degree[i] == 1) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            res = new ArrayList<>();
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                int next = queue.poll();
+                res.add(next);
+                for (int k : graph[next]) {
+                    degree[k]--;
+                    if (degree[k] == 1) {
+                        queue.offer(k);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
