85f7563d34422a6eb13c6545e0dc02edd57b1d7c
xzheng <xin.zheng@eisgroup.com>
2020-07-20 14:30:22 +0800

    add weekly contest 156

--- leetcode/src/com/xinzhe/contest/weekly/season04/weekly156/Leetcode_weekly_15603.java
+++ leetcode/src/com/xinzhe/contest/weekly/season04/weekly156/Leetcode_weekly_15603.java
@@ -1,19 +1,58 @@
 package com.xinzhe.contest.weekly.season04.weekly156;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author Xin
  * @create 2020/7/17
- * Title :
- * Description :
- * link :
- * Level : Easy
- * Comment 156周赛01
+ * Title : 1209. 删除字符串中的所有相邻重复项 II
+ * Description : 给你一个字符串 s，「k 倍重复项删除操作」将会从 s 中选择 k 个相邻且相等的字母，并删除它们，使被删去的字符串的左侧和右侧连在一起。
+ *              你需要对 s 重复进行无限次这样的删除操作，直到无法继续为止。在执行完所有删除操作后，返回最终得到的字符串。本题答案保证唯一。
+ * link : https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string-ii
+ * Level : Medium
+ * Comment 156周赛03
  */
 
 public class Leetcode_weekly_15603 {
 
     public static void main(String[] args) {
         Leetcode_weekly_15603 lc = new Leetcode_weekly_15603();
+        System.out.println(lc.removeDuplicates("deeedbbcccbdaa", 3));
     }
+    public String removeDuplicates(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        Deque<Character> stack = new LinkedList<>();
+        Deque<Integer> count = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            if(stack.isEmpty() || count.peek() < k) {
+                if(!stack.isEmpty()) {
+                    char c = stack.peek();
+                    int pre = count.peek();
+                    if(arr[i] == c) {
+                        count.pop();
+                        count.push(pre + 1);
+                    } else {
+                        count.push(1);
+                    }
 
+                } else {
+                    count.push(1);
+                }
+                stack.push(arr[i]);
+            }
+            if(count.peek() == k) {
+                int pop = count.pop();
+                while (pop-- > 0) {
+                    stack.pop();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
+    }
 }
--- leetcode/src/com/xinzhe/contest/weekly/season04/weekly156/Leetcode_weekly_15604.java
+++ leetcode/src/com/xinzhe/contest/weekly/season04/weekly156/Leetcode_weekly_15604.java
@@ -1,19 +1,92 @@
 package com.xinzhe.contest.weekly.season04.weekly156;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * @Author Xin
  * @create 2020/7/17
- * Title :
- * Description :
- * link :
- * Level : Easy
- * Comment 156周赛01
+ * Title : 1210. 穿过迷宫的最少移动次数
+ * Description : 你还记得那条风靡全球的贪吃蛇吗？
+ *          我们在一个 n*n 的网格上构建了新的迷宫地图，蛇的长度为 2，也就是说它会占去两个单元格。蛇会从左上角（(0, 0) 和 (0, 1)）开始移动。
+ *          我们用 0 表示空单元格，用 1 表示障碍物。蛇需要移动到迷宫的右下角（(n-1, n-2) 和 (n-1, n-1)）。
+ * link : https://leetcode-cn.com/problems/minimum-moves-to-reach-target-with-rotations/
+ * Level : Hard
+ * Comment 156周赛04
  */
 
 public class Leetcode_weekly_15604 {
 
     public static void main(String[] args) {
         Leetcode_weekly_15604 lc = new Leetcode_weekly_15604();
+        int[][] arr = {{0,0,0,0,0,1}, {1,1,0,0,1,0},{0,0,0,0,1,1}, {0,0,1,0,1,0}, {0,1,1,0,0,0}, {0,1,1,0,0,0}};
+        System.out.println(lc.minimumMoves(arr));
+    }
+    public int minimumMoves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        POS start = new POS(0, 1, 0, 0);
+        LinkedList<POS> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            POS cur = queue.poll();
+            if (cur.x == m - 1 && cur.y == n - 1 && cur.to == 0) return cur.step;
+            String s = String.valueOf(cur.x) + cur.y + cur.to;
+            if (visited.contains(s)) continue;
+            visited.add(s);
+            int newY = cur.y + 1, newX = cur.x + 1;
+            if (cur.to == 0) {
+                if (newY < n && grid[cur.x][newY] == 0) {
+                    queue.offer(new POS(cur.x, newY, 0, cur.step + 1));
+                }
+                if (newX < m && grid[newX][cur.y] == 0 && grid[newX][cur.y - 1] == 0) {
+                    queue.offer(new POS(newX, cur.y - 1, 1, cur.step + 1));
+                    queue.offer(new POS(newX, cur.y, 0, cur.step + 1));
+                }
+            } else {
+                if (newX < m && grid[newX][cur.y] == 0) {
+                    queue.offer(new POS(newX, cur.y, 1, cur.step + 1));
+                }
+                if (newY < n && grid[cur.x][newY] == 0 && grid[cur.x - 1][newY] == 0) {
+                    queue.offer(new POS(cur.x - 1, newY, 0, cur.step + 1));
+                    queue.offer(new POS(cur.x, newY, 1, cur.step + 1));
+                }
+            }
+        }
+        return -1;
     }
 
+    class POS {
+        int x;
+        int y;
+        int to;
+        int step;
+
+        public POS(int x, int y, int to, int step) {
+            this.x = x;
+            this.y = y;
+            this.to = to;
+            this.step = step;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            POS pos = (POS) o;
+            return x == pos.x &&
+                    y == pos.y &&
+                    to == pos.to;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, to);
+        }
+    }
 }
