aa04fb3f2241e4eed6a295c036ad6bfe5b98a67a
xzheng <xin.zheng@eisgroup.com>
2020-08-21 16:52:34 +0800

    add greedy and stack related issues

---
+++ src/com/xinzhe/categories/solutions/greedy/hard/Leetcode321.java
@@ -0,0 +1,101 @@
+package com.xinzhe.categories.solutions.greedy.hard;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Xin
+ * @date 2020/8/21
+ * Title : 321. 拼接最大数
+ * Description : 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，
+ *              要求从同一个数组中取出的数字保持其在原数组中的相对顺序。求满足该条件的最大数。结果返回一个表示该最大数的长度为 k 的数组。
+ * link : https://leetcode-cn.com/problems/create-maximum-number
+ * Level : Hard
+ */
+//todo need to review
+public class Leetcode321 {
+    public static void main(String[] args) {
+        Leetcode321 lc = new Leetcode321();
+        int[] a1 = {6,7};
+        int[] a2 = {};
+        System.out.println(Arrays.toString(lc.maxNumber(a1, a2, 2)));
+    }
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] res = new int[k];
+        if(m + n < k) return res;
+
+        // i 的范围与 m n k 相关
+        for (int i = Math.max(0, k-n); i <= Math.min(m, k); ++i) {
+            List<Integer> l1 = getTopK(nums1, i);
+            List<Integer> l2 = getTopK(nums2, k-i);
+            int[] merge = merge(l1, l2);
+            if(compare(merge, res)) res = merge;
+        }
+        return res;
+    }
+
+    private boolean compare(int[] n1, int[] n2) {
+        for (int i = 0; i < n1.length; ++i) {
+            if(n1[i] != n2[i]) {
+                return n1[i] > n2[i];
+            }
+        }
+        return false;
+    }
+
+    // 相同的时候 需要继续比较后继元素的大小，选择大的那个
+    private int[] merge(List<Integer> l1, List<Integer> l2) {
+        int m = l1.size(), n = l2.size();
+        int[] res = new int[m+n];
+        int p = 0, q = 0, k = 0;
+        while (p < m && q < n) {
+            if(l1.get(p) > l2.get(q)) {
+                res[k++] = l1.get(p++);
+            }else if(l1.get(p) < l2.get(q)){
+                res[k++] = l2.get(q++);
+            } else {
+                int i=p, j= q;
+                while (i < m && j < n && l1.get(i).equals(l2.get(j))){
+                    i++; j++;
+                }
+                if(i == m) res[k++] = l2.get(q++);
+                else if(j == n) res[k++] = l1.get(p++);
+                else if(l1.get(i) > l2.get(j)) res[k++] = l1.get(p++);
+                else res[k++] = l2.get(q++);
+            }
+        }
+        while (p < m) { res[k++] = l1.get(p++); }
+        while (q < n) { res[k++] = l2.get(q++); }
+        return res;
+    }
+
+    //count ： 待删除的数目
+    private List<Integer> getTopK(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        if(k == 0) return res;
+        int n = nums.length;
+        if(k == n) return Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int count = n - k;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num && count > 0) {
+                stack.pop();
+                count--;
+            }
+            if (stack.size() < k) {
+                stack.push(num);
+            } else {
+                count--;
+            }
+        }
+        while (!stack.isEmpty()) {
+            res.add(0, stack.pop());
+        }
+        return res;
+    }
+}
---
+++ src/com/xinzhe/categories/solutions/greedy/medium/Leetcode402.java
@@ -0,0 +1,42 @@
+package com.xinzhe.categories.solutions.greedy.medium;
+
+import java.util.ArrayDeque;
+
+/**
+ * @author Xin
+ * @date 2020/8/21
+ * Title : 402. 移掉K位数字
+ * Description : 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+ *              num 的长度小于 10002 且 ≥ k。num 不会包含任何前导零。
+ * link : https://leetcode-cn.com/problems/remove-k-digits
+ * Level : Medium
+ */
+//todo need to review
+public class Leetcode402 {
+    public static void main(String[] args) {
+        Leetcode402 lc = new Leetcode402();
+        System.out.println(lc.removeKdigits("1234567890", 9));
+    }
+    public String removeKdigits(String num, int k) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < num.length(); ++i) {
+            while (!stack.isEmpty() && stack.peek() > num.charAt(i) && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+        }
+        while (!stack.isEmpty() && k-- > 0) {
+            stack.pop();
+        }
+        if(stack.isEmpty()) return "0";
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        while (sb.length() != 1 && sb.lastIndexOf("0") == sb.length()-1) {
+            sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.reverse().toString();
+    }
+}
