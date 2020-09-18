bcb54f21178d0342cd7620ecc0dd841328f0ceee
xzheng <xin.zheng@eisgroup.com>
2020-09-18 17:39:48 +0800

    review weekly contest 183

--- src/com/xinzhe/contest/weekly/season04/weekly183/Leetcode_weekly_18304.java
+++ src/com/xinzhe/contest/weekly/season04/weekly183/Leetcode_weekly_18304.java
@@ -1,45 +1,20 @@
 package com.xinzhe.contest.weekly.season04.weekly183;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author Xin
- * @date 2020/4/5 10:28
- * Title : 1403. 非递增顺序的最小子序列
- * Description : 给你一个数组 nums，请你从中抽取一个子序列，满足该子序列的元素之和 严格 大于未包含在该子序列中的各元素之和。
- *              如果存在多个解决方案，只需返回 长度最小 的子序列。如果仍然有多个解决方案，则返回 元素之和最大 的子序列。
- *              与子数组不同的地方在于，「数组的子序列」不强调元素在原数组中的连续性，也就是说，它可以通过从数组中分离一些（也可能不分离）元素得到。
- * link : https://leetcode-cn.com/problems/minimum-subsequence-in-non-increasing-order/
- * Level : Easy
- * Comment : 183周赛01
+ * @date 2020/4/5 10:27
+ * Title : 1406. 石子游戏 III
+ * Description : Alice 和 Bob 用几堆石子在做游戏。几堆石子排成一行，每堆石子都对应一个得分，由数组 stoneValue 给出。
+ *          Alice 和 Bob 轮流取石子，Alice 总是先开始。在每个玩家的回合中，该玩家可以拿走剩下石子中的的前 1、2 或 3 堆石子 。比赛一直持续到所有石头都被拿走。
+ *          每个玩家的最终得分为他所拿到的每堆石子的对应得分之和。每个玩家的初始分数都是 0 。比赛的目标是决出最高分，得分最高的选手将会赢得比赛，比赛也可能会出现平局。
+ *          假设 Alice 和 Bob 都采取 最优策略 。如果 Alice 赢了就返回 "Alice" ，Bob 赢了就返回 "Bob"，平局（分数相同）返回 "Tie" 。
+ * link : https://leetcode-cn.com/problems/stone-game-iii
+ * Level : Hard
+ * Comment : 183周赛04
  */
+//todo undo
 public class Leetcode_weekly_18304 {
-    public static void main(String[] args) {
-        int[] a = {4,4,7,6,7};
-        System.out.println(minSubsequence(a));
-    }
-    public static List<Integer> minSubsequence(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        int n = nums.length;
-        if(n == 0) return list;
-        if(n == 1) {
-            list.add(nums[0]);
-            return list;
-        }
-        Arrays.sort(nums);
-        int sum = Arrays.stream(nums).sum();
-        int target = sum /2 +1;
-        int res = 0;
-        for(int i = n-1; i >= 0; --i) {
-            if(res >= target){
-                break;
-          } else {
-                res += nums[i];
-                list.add(nums[i]);
-          }
-        }
-        return list;
+    public String stoneGameIII(int[] stoneValue) {
+        return "";
     }
 }
