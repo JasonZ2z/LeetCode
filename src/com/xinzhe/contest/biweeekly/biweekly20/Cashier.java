512ae3408c849f4e3e9e184a38ca7f17f771bd23
xzheng <371635013@qq.com>
2020-10-03 11:34:22 +0800

    add weekly contest 148

--- src/com/xinzhe/contest/biweeekly/biweekly20/Cashier.java
+++ src/com/xinzhe/contest/biweeekly/biweekly20/Cashier.java
@@ -7,13 +7,13 @@ import java.util.Map;
  * @author Xin
  * @date 2020/5/18
  * Title : 1357. 每隔 n 个顾客打折
- * Description : 超市里正在举行打折活动，每隔 n 个顾客会得到 discount 的折扣。
- * 超市里有一些商品，第 i 种商品为 products[i] 且每件单品的价格为 prices[i] 。
- * 结账系统会统计顾客的数目，每隔 n 个顾客结账时，该顾客的账单都会打折，折扣为 discount （也就是如果原本账单为 x ，那么实际金额会变成 x - (discount * x) / 100 ），然后系统会重新开始计数。
- * 顾客会购买一些商品， product[i] 是顾客购买的第 i 种商品， amount[i] 是对应的购买该种商品的数目。
- * 请你实现 Cashier 类：
- * Cashier(int n, int discount, int[] products, int[] prices) 初始化实例对象，参数分别为打折频率 n ，折扣大小 discount ，超市里的商品列表 products 和它们的价格 prices 。
- * double getBill(int[] product, int[] amount) 返回账单的实际金额（如果有打折，请返回打折后的结果）。返回结果与标准答案误差在 10^-5 以内都视为正确结果。
+ * Description : 超市里正在举行打折活动，每隔 n 个顾客会得到 discount 的折扣。
+ * 超市里有一些商品，第 i 种商品为 products[i] 且每件单品的价格为 prices[i] 。
+ * 结账系统会统计顾客的数目，每隔 n 个顾客结账时，该顾客的账单都会打折，折扣为 discount （也就是如果原本账单为 x ，那么实际金额会变成 x - (discount * x) / 100 ），然后系统会重新开始计数。
+ * 顾客会购买一些商品， product[i] 是顾客购买的第 i 种商品， amount[i] 是对应的购买该种商品的数目。
+ * 请你实现 Cashier 类：
+ * Cashier(int n, int discount, int[] products, int[] prices) 初始化实例对象，参数分别为打折频率 n ，折扣大小 discount ，超市里的商品列表 products 和它们的价格 prices 。
+ * double getBill(int[] product, int[] amount) 返回账单的实际金额（如果有打折，请返回打折后的结果）。返回结果与标准答案误差在 10^-5 以内都视为正确结果。
  * link : https://leetcode-cn.com/problems/apply-discount-every-n-orders
  * Level : Medium
  * Comment 20双周赛02
