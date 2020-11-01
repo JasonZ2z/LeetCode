c7d63a69315f99a2b30b1ec10fa8484c572a7347
xzheng <371635013@qq.com>
2020-11-01 20:56:38 +0800

    daily checkin

--- src/com/xinzhe/categories/solutions/dp/exist/Leetcode140.java
+++ src/com/xinzhe/categories/solutions/dp/exist/Leetcode140.java
@@ -1,14 +1,18 @@
 package com.xinzhe.categories.solutions.dp.exist;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Xin
  * @date 2020/7/31
  * Title : 140. 单词拆分 II
  * Description : 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
- *              分隔时可以重复使用字典中的单词。
- *              你可以假设字典中没有重复的单词。
+ *              分隔时可以重复使用字典中的单词。你可以假设字典中没有重复的单词。
  * link : https://leetcode-cn.com/problems/word-break-ii
  * Level : Hard
  * tag : 记忆化回溯
@@ -17,33 +21,40 @@ import java.util.*;
 // todo need to review
 public class Leetcode140 {
     public static void main(String[] args) {
-        String s= "c";
-        List<String> list = Arrays.asList("c,", "d");
-        //System.out.println(wordBreak(s, list));
+        String s= "catsanddog";
+        List<String> list = Arrays.asList("cat", "cats", "and", "sand", "dog");
+        Leetcode140 lc = new Leetcode140();
+        System.out.println(lc.wordBreak(s, list));
     }
-    int max = 0;
-    HashMap<Integer, List<String>> map = new HashMap<>();
+
+    int max;
+    HashSet<String> set = new HashSet<>();
     public List<String> wordBreak(String s, List<String> wordDict) {
-        this.max = wordDict.stream().max(Comparator.comparingInt(String::length)).get().length();
-        return dfs(s, wordDict, 0);
-
-    }
-
-    private List<String> dfs(String s, List<String> wordDict, int i) {
-        if(map.containsKey(i)) return map.get(i);
-        List<String> res = new ArrayList<>();
-        if(i == s.length()) {
-            res.add("");
+        this.max = 0;
+        for(String word : wordDict) {
+            set.add(word);
+            max = Math.max(max, word.length());
         }
-        for (int j = 0; j <= s.length() && j <= max; ++j) {
-            if(wordDict.contains(s.substring(i, j))) {
-                List<String> list = dfs(s, wordDict, j);
-                for (String l : list) {
-                    res.add((s.substring(i,j) + " " + l).trim());
+        return dfs(s, 0);
+    }
+    Map<Integer, List<String>> map = new HashMap<>();
+    private List<String> dfs(String s, int index) {
+        if(map.containsKey(index)) return map.get(index);
+        List<String> res = new ArrayList<>();
+        if(index == s.length()) {
+            res.add("");
+            return res;
+        }
+        for(int i=1; i + index <= s.length() && i <= max; i++) {
+            String tmp = s.substring(index, i + index);
+            if(set.contains(tmp)) {
+                List<String> sub = dfs(s, i + index);
+                for(String str : sub) {
+                    res.add((tmp + " " + str).trim());
                 }
             }
         }
-        map.put(i, res);
+        map.put(index, res);
         return res;
     }
 }
