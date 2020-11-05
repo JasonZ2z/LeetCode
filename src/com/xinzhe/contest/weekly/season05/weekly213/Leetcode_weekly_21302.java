7df5384cd6fb692f6f6f27d2a7caf993123a1b1d
xzheng <xin.zheng@eisgroup.com>
2020-11-05 15:51:04 +0800

    review weekly contest 213

--- src/com/xinzhe/contest/weekly/season05/weekly213/Leetcode_weekly_21302.java
+++ src/com/xinzhe/contest/weekly/season05/weekly213/Leetcode_weekly_21302.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 /**
  * @author Xin
  * @date 2020/11/01
- * Title :
- * Description :
- * link :
- * Level : Easy
+ * Title : 1641. 统计字典序元音字符串的数目
+ * Description : 给你一个整数 n，请返回长度为 n 、仅由元音 (a, e, i, o, u) 组成且按 字典序排列 的字符串数量。
+ *              字符串 s 按 字典序排列 需要满足：对于所有有效的 i，s[i] 在字母表中的位置总是与 s[i+1] 相同或在 s[i+1] 之前。
+ * link : https://leetcode-cn.com/problems/count-sorted-vowel-strings
+ * Level : Medium
  */
 public class Leetcode_weekly_21302 {
     public static void main(String[] args) {
--- src/com/xinzhe/contest/weekly/season05/weekly213/Leetcode_weekly_21304.java
+++ src/com/xinzhe/contest/weekly/season05/weekly213/Leetcode_weekly_21304.java
@@ -1,16 +1,18 @@
 package com.xinzhe.contest.weekly.season05.weekly213;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Xin
  * @date 2020/11/01
- * Title :
- * Description :
- * link :
- * Level : Easy
+ * Title : 1643. 第 K 条最小指令
+ * Description : Bob 站在单元格 (0, 0) ，想要前往目的地 destination ：(row, column) 。他只能向 右 或向 下 走。你可以为 Bob 提供导航 指令 来帮助他到达目的地 destination 。
+ *              指令 用字符串表示，其中每个字符：'H' ，意味着水平向右移动'V' ，意味着竖直向下移动
+ *              能够为 Bob 导航到目的地 destination 的指令可以有多种，例如，如果目的地 destination 是 (2, 3)，"HHHVV" 和 "HVHVH" 都是有效 指令 。
+ *              然而，Bob 很挑剔。因为他的幸运数字是 k，他想要遵循 按字典序排列后的第 k 条最小指令 的导航前往目的地 destination 。k 的编号 从 1 开始 。
+ *              给你一个整数数组 destination 和一个整数 k ，请你返回可以为 Bob 提供前往目的地destination 导航的 按字典序排列后的第 k 条最小指令 。
+ * link : https://leetcode-cn.com/problems/kth-smallest-instructions
+ * Level : Hard
  */
+//todo too hard to solve
 public class Leetcode_weekly_21304 {
     public static void main(String[] args) {
         Leetcode_weekly_21304 lc = new Leetcode_weekly_21304();
@@ -18,38 +20,31 @@ public class Leetcode_weekly_21304 {
         System.out.println(lc.kthSmallestPath(arr, 1));
         System.out.println("11".substring(1,1));
     }
-    Map<int[], Long> map = new HashMap<>();
+    int[][] nCr;
     public String kthSmallestPath(int[] destination, int k) {
-        int m = destination[0], n = destination[1];
-        count(0,0, m, n);
-        StringBuilder sb = new StringBuilder();
-        dfs(0,0,m,n, k, sb);
-        return sb.toString();
+        nCr = new int[30][16];
+        return dfs(destination[1], destination[0], k);
     }
 
-    private void dfs(int i, int j, int m, int n,  long k, StringBuilder s) {
-        if(k == 0) return;
-        long h = 0;
-        if(j + 1 < n) {
-            h = map.getOrDefault(new int[]{i, j+1} ,0L);
+    private String dfs(int H, int V, int k) {
+        if (H == 0 && V == 0) return "";
+        else if (H == 0) {
+            return 'V'+ dfs(H, V - 1, k);
+        } else if (V == 0) {
+            return 'H' + dfs(H - 1, V, k);
+        } else {
+            int threshold = nCr(H - 1 + V, V);
+            if (k <= threshold) {
+               return 'H'+ dfs(H - 1, V, k);
+            } else {
+                return 'V' + dfs(H, V - 1, k - threshold);
+            }
         }
-        if(i + 1 < m && k > h) {
-            dfs(i+1, j, m, n, k - h, s.append("V"));
-        } else if(j + 1 < n){
-            dfs(i, j+1, m, n, k, s.append("H"));
-        }
-
     }
 
-
-    private long count(int i, int j, int m, int n) {
-        if(i == m-1 && j == n-1) {
-            map.put(new int[]{i, j}, 0L);
-            return 0L;
-        }
-        long left = i < m ? count(i + 1, j, m, n) : 0L;
-        long right = j < n ? count(i, j + 1, m, n) : 0L;
-        map.put(new int[]{i, j}, left + right);
-        return left + right;
+    private int nCr(int n, int r) {
+        if (r == 0 || r == n) return 1;
+        if (nCr[n][r] > 0) return nCr[n][r];
+        return nCr[n][r] = nCr(n - 1, r) + nCr(n - 1, r - 1);
     }
 }
