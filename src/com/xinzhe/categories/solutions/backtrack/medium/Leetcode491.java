06a5f460169a0752b36453d29e42f04e23de3a4e
xzheng <xin.zheng@eisgroup.com>
2020-12-30 13:46:06 +0800

    daily checkin

--- src/com/xinzhe/categories/solutions/backtrack/medium/Leetcode491.java
+++ src/com/xinzhe/categories/solutions/backtrack/medium/Leetcode491.java
@@ -13,29 +13,76 @@ import java.util.Set;
  * link : https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
  * Level : Medium
  */
+
+//todo need to review
 public class Leetcode491 {
     public static void main(String[] args) {
+        Leetcode491 lc = new Leetcode491();
         int[] arr = {4,6,7,7};
-        System.out.println(findSubsequences(arr));
-    }
-    private static Set<List<Integer>> res = new HashSet<>();
-    public static List<List<Integer>> findSubsequences(int[] nums) {
-        if(nums.length < 2) return new ArrayList<>();
-        bfs(nums, new ArrayList<>(), 0);
-        return new ArrayList<>(res);
+        System.out.println(lc.findSubsequences(arr));
+        res.clear();
+        System.out.println(lc.findSubsequences2(arr));
+        res.clear();
+        System.out.println(lc.findSubsequences3(arr));
     }
 
-    private static void bfs(int[] nums, ArrayList<Integer> track, int start) {
-        if(track.size() >= 2){
-            res.add(new ArrayList<>(track));
-        }
-        if(start > nums.length) return;
-        for (int i = start; i < nums.length; i++) {
-            if(track.size() == 0 || nums[i] >= track.get(track.size()-1)) {
-                track.add(nums[i]);
-                bfs(nums, track, i + 1);
-                track.remove(track.size() - 1);
+    static List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        dfs(nums, 0, new ArrayList<>());
+        return res;
+    }
+
+    private void dfs(int[] nums, int index, List<Integer> list) {
+        if(list.size() > 1) res.add(new ArrayList<>(list));
+        if(index == nums.length) return;
+        Set<Integer> set = new HashSet<>();
+        for(int i=index; i<nums.length; i++) {
+            if(set.contains(nums[i]))continue;
+            if(list.isEmpty() || nums[i] >= list.get(list.size() -1)) {
+                set.add(nums[i]);
+                list.add(nums[i]);
+                dfs(nums, i+1, list);
+                list.remove(list.size() - 1);
             }
+        }
+    }
+
+    public List<List<Integer>> findSubsequences2(int[] nums) {
+        dfs2(nums, 0,  new ArrayList<>());
+        return res;
+    }
+    private void dfs2(int[] nums, int cur,  List<Integer> list) {
+        if(cur == nums.length) {
+            if(list.size() > 1) res.add(new ArrayList<>(list));
+            return;
+        }
+        if(list.isEmpty() || nums[cur] >= list.get(list.size() - 1)) {
+            list.add(nums[cur]);
+            dfs2(nums, cur + 1, list);
+            list.remove(list.size() - 1);
+        }
+        if(list.isEmpty() || nums[cur] != list.get(list.size() - 1)) {
+            dfs2(nums, cur + 1, list);
+        }
+    }
+
+
+    public List<List<Integer>> findSubsequences3(int[] nums) {
+        dfs(nums, 0, Integer.MIN_VALUE, new ArrayList<>());
+        return res;
+    }
+    private void dfs(int[] nums, int cur, int pre, List<Integer> list) {
+        if(cur == nums.length) {
+            if(list.size() > 1) res.add(new ArrayList<>(list));
+            return;
+        }
+        if(nums[cur] >= pre) {
+            list.add(nums[cur]);
+            dfs(nums, cur + 1, nums[cur], list);
+            list.remove(list.size() - 1);
+        }
+        if(nums[cur] != pre) {
+            dfs(nums, cur + 1, pre, list);
         }
     }
 }
