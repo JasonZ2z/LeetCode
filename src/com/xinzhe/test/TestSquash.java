79b4651ddabacf3301f4ee30e93dca65350c492f
xzheng <371635013@qq.com>
2021-02-25 21:06:51 +0800

    test squash-master

---
+++ src/com/xinzhe/test/TestSquash.java
@@ -0,0 +1,13 @@
+package com.xinzhe.test;
+
+import java.util.HashMap;
+
+/**
+ * @author xzheng
+ * @create 2021/2/25
+ */
+public class TestSquash {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+    }
+}
